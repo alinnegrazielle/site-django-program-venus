48c727ee19e72cb8a599d24e77b186801ece6195
alinne <alinnegraziellecontato@gmail.com>
2021-05-17 14:08:06 -0300

    projeto iniciado

--- projeto_venus/projeto/paginas/urls.py
+++ projeto_venus/projeto/paginas/urls.py
@@ -3,7 +3,6 @@ from .views import IndexView, DiretoriaView, AcPrivView, AcPubView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='inicio'),
-    path('diretoria', DiretoriaView.as_view(), name='diretoria'),
     path('acervoprivado', AcPrivView.as_view(), name='acpriv'),
     path('acervopublico', AcPubView.as_view(), name='acpub'),
 ]
