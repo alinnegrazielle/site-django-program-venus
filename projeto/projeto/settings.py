0cb638b6a19697a152c3bdf7d3f482511d9f4dc1
unknown <manununes@hotmail.com.br>
2021-06-10 08:38:19 -0300

    Ajustes: Conexão com o banco de dados Postgres

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_venus',
+        'USER': 'dev',
+        'PASSWORD': 'acessodev415263.',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
