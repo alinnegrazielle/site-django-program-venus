7564c5e7cbc533c12a625c022584b19a93e22928
alinne <alinnegraziellecontato@gmail.com>
2021-06-22 16:51:15 -0300

    Revert "Ajustes: Conexão com o banco de dados Postgres"
    This reverts commit 0cb638b6a19697a152c3bdf7d3f482511d9f4dc1.

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_venus',
-        'USER': 'dev',
-        'PASSWORD': 'acessodev415263.',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
